4d24f95d003215b7037c3fd3147d656847f5d2be
Utkarsh13124 <utkarshp579@gmail.com>
2025-07-24 13:48:58 +0530

    Tessting done , 1 bug found -> editing listing , if image not filled , it leads to error , should filled same previous again

---
+++ AirMojo Notes/Phase3_partD.js
@@ -0,0 +1,33 @@
+/*
+    ! Using Mongo Atlas.
+        hum Apne DB ko store krwana chate hi . Online
+
+        MongDB --> Deploy  a multi-cloud database.
+
+        Main theme is to connect :- Mongo Atlas with app.js
+*/
+
+/*
+    ! Mongo Session Store
+        Jo humne express sessions use kiya tha , pehle wo sirf debugging ke liye tha , nott for development purpose.
+
+*/
+
+/*
+    ! Deployment with render
+        add engines in package.json
+
+
+        Naye features kaise deploy honge. 
+            local system connected to github , github connected to Render.
+            we push codes to github , and fetch new copy from render.
+*/
+
+/*
+    connect to github 
+    use render , 
+    set upenvironment variable at render
+    give permission to Render Ip addresses at Atlas , as they request DB from Atlas.
+
+
+*/
